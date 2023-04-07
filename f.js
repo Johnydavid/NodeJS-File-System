197aed95e6a18c6065b00a8ba4e443c22c44bdc1
Johnydavid <dropin2johny@gmail.com>
2023-04-07 17:48:30 +0530

    c

--- f.js
+++ f.js
@@ -2,3 +2,15 @@ const n = require('./files');
 
 // return n;
 
+const router = require('express').Router();
+const path = require("path");
+const dirPath = path.join(__dirname, "date_time");
+
+router.get('/', function(req, res){
+    // res.send(n);
+    res.sendFile(dirPath + "/" + "7-4-2023 17-3-5.txt" )
+    
+});
+
+module.exports = router;
+
